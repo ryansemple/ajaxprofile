31eb4aed113d14dd6ca0f47e68b7c5c8179c8e5f
ryansemple <seafoam.green91@gmail.com>
2015-04-27 00:34:36 -0700

    changed so it waits a while before saving

--- ajax/Scripts/profile.js
+++ ajax/Scripts/profile.js
@@ -2,10 +2,19 @@
     $bioFlag = false;
     var webApiURL = "http://" + location.host + "/api/API/";
     $bioInput = $("#biography_input");
+    var timer;
     $bioInput.on('input', function () {
+        clearTimeout(timer);
+        startCount();
+    });
+    function startCount() {
+        timer = setTimeout(setBio, 3000);
+    }
+    function setBio() {
+        alert("writing to db");
         $bioInputValue = $bioInput.val();
         var setBiographyLink = webApiURL + "?biographyContent=" + $bioInputValue;
         setBiographyLink = encodeURI(setBiographyLink.toString());
         $.getJSON(setBiographyLink, null, function (data) { });
-    });
+    }
 });
